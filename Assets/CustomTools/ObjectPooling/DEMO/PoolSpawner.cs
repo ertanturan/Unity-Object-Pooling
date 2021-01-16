096fdbcb2466f4aec7e5bbc4ff13fc80e426b16a
Ertan Turan <ertanturan@outlook.com>
2021-01-16 23:24:39 +0300

    spawn and despawn demonstration finished

---
+++ Assets/CustomTools/ObjectPooling/DEMO/PoolSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CustomTools.ObjectPooling.Scripts.ObjectPool;
+using UnityEngine;
+using Zenject;
+
+public class PoolSpawner : MonoBehaviour
+{
+
+    private List<IPooledObject> _cubes = new List<IPooledObject>();
+    private ObjectPooler _pooler;
+
+    [SerializeField] private Transform _spawnPosition;
+    
+    [Inject]
+    private void Construct(ObjectPooler pooler)
+    {
+        _pooler = pooler;
+    }
+    
+    
+    public void SpawnCube()
+    {
+        GameObject obj = _pooler.SpawnFromPool(PooledObjectType.Cube,
+            _spawnPosition.position, Quaternion.identity);
+        
+        _cubes.Add(obj.GetComponent<IPooledObject>());
+    }
+
+    public void DespawnAnyCube()
+    {
+        if (_cubes.Count>0)
+        {
+
+            _cubes[0].Despawn();
+            _cubes.RemoveAt(0);
+        }
+    }
+    
+}
--- Assets/CustomTools/ObjectPooling/Scripts/ObjectPool/IPooledObject.cs
+++ Assets/CustomTools/ObjectPooling/Scripts/ObjectPool/IPooledObject.cs
@@ -1,9 +1,11 @@
 ﻿using CustomTools.ObjectPooling.Scripts.ObjectPool;
+using UnityEngine;
 
 public interface IPooledObject
 {
     PooledObjectType PoolType { get; set; }
-     // ObjectPooler ObjectPooler { get;  set; }
+
+
     void Construct(ObjectPooler pooler);
     void OnObjectSpawn();
     void OnObjectDespawn();
