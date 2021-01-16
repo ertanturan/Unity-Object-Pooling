7f2561e727d71b81cfd1588e1ede3ae729a62226
Ertan Turan <ertanturan@outlook.com>
2021-01-16 23:48:07 +0300

    addendum

--- Assets/CustomTools/ObjectPooling/Scripts/ObjectPool/IPooledObject.cs
+++ Assets/CustomTools/ObjectPooling/Scripts/ObjectPool/IPooledObject.cs
@@ -1,12 +1,14 @@
 ﻿using CustomTools.ObjectPooling.Scripts.ObjectPool;
 using UnityEngine;
+using Zenject;
 
 public interface IPooledObject
 {
     PooledObjectType PoolType { get; set; }
 
      ObjectPooler Pooler { get; }
-
+     
+     [Inject]
     void Construct(ObjectPooler pooler);
     void OnObjectSpawn();
     void OnObjectDespawn();
